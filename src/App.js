5c08f66cdf17c9fe9700bd72813884b1f894729a
Stevie Rosen <rosenstevie1998@gmail.com>
2020-02-11 16:17:18 -0500

    navigation-structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, {Component} from 'react';
 import "./App.css";
+import { Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default class App extends Component{
   render(){
-    return <div>Fisher Bookstore</div>;
+    return (
+    <div className= "App container">
+      <Navbar bg="dark" expand ="lg">
+        <Navbar.Brand>
+          <Link to= "/">Fisher Bookstore</Link>
+        </Navbar.Brand>
+      </Navbar>
+    </div>
+      );
   }
 };
